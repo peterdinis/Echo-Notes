d885aef26c941de39628eb31d0a508a41fe8ab4e
peterdinis <petter1@pobox.sk>
2025-05-25 14:27:12 +0200

    infra: Adding storybook WIP

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    {
+      "name": "@storybook/addon-essentials",
+      "options": {
+        "docs": false
+      }
+    },
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
+  }
+};
+export default config;
